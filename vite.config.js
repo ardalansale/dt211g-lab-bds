05467e449bf491a2c70952924ed77ed4dfba3c84
Ardalan Sale <114071686+ardalansale@users.noreply.github.com>
2026-03-13 22:51:37 +0100

    Install NPM sharp for jpg omptimization
    Change vite.config.js to resolve netlify finding the right page

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite'
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
+import { resolve } from 'path'
 
 export default defineConfig({
-    plugins: [
-        ViteImageOptimizer({
-            png: { quality: 80 },
-            jpg: { quality: 80 },
-            jpeg: { quality: 80 },
-            webp: { quality: 80 },
-        })
-    ]
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        sass: resolve(__dirname, 'sass.html'),
+      }
+    }
+  },
+  
+  plugins: [
+    ViteImageOptimizer({
+      svg: false,
+      png: { quality: 80 },
+      jpg: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { quality: 80 },
+    })
+  ]
 })
