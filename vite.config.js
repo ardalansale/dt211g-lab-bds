0b61b3897948ef905388d535ac1fb742d82f478e
Ardalan Sale <114071686+ardalansale@users.noreply.github.com>
2026-03-16 23:26:31 +0100

    Create animations.scss and animering.html
    Add fade in animation to h1 in animering-page

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         sass: resolve(__dirname, 'sass.html'),
+        animering: resolve(__dirname, 'animering.html'),
       }
     }
   },
